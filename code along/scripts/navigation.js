6855a666870083772027812bba13b51005998a7a
learsiml-afk <learsiml@gmail.com>
2026-07-01 23:18:01 -0700

    java

--- code along/scripts/navigation.js
+++ code along/scripts/navigation.js
@@ -1,5 +1,10 @@
 const navbuttom = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
 
 navbuttom.addEventListener('click',() => {
     navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+
 });
+
+
